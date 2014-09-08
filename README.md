1ab0499e9fd53d1d49ade781779c1fe3ae672169
Matt Thomson <matt.thomson@mendeley.com>
2014-09-08 11:03:11 +0100

    Migrate app to use SDK.

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Mendeley API Python Example #
 
-This is a simple example of an application that consumes the [Mendeley](http://www.mendeley.com) API.  For more information on the API, see the [developer portal](http://dev.mendeley.com).
+This is a simple example of an application that consumes the [Mendeley](http://www.mendeley.com) API, using the [Python SDK](http://mendeley-python.readthedocs.org/).
+
+For more information on the API, see the [developer portal](http://dev.mendeley.com).
 
 ## About the application ##
 
 The application is a website that allows you to see the documents in your Mendeley library, together with their annotations.  It also allows you to look up a document by [DOI](http://www.doi.org/).
 
-It's built with [Flask](http://flask.pocoo.org/) and [Jinja2](http://jinja.pocoo.org/docs/).  Authenticated HTTP requests are handled by [Requests](http://docs.python-requests.org/) and [OAuthLib](https://oauthlib.readthedocs.org/).
+It's built with [Flask](http://flask.pocoo.org/) and [Jinja2](http://jinja.pocoo.org/docs/).
 
 ## How to run ##
 
--- mendeley-example.py
+++ mendeley-example.py
@@ -1,9 +1,8 @@
-from urllib import urlencode
-
 from flask import Flask, redirect, render_template, request, session
-from requests_oauthlib import OAuth2Session
-from oauthlib.oauth2.rfc6749.errors import OAuth2Error
 import yaml
+
+from mendeley import Mendeley
+from mendeley.session import MendeleySession
 
 
 with open('config.yml') as f:
@@ -11,12 +10,11 @@ with open('config.yml') as f:
 
 REDIRECT_URI = 'http://localhost:5000/oauth'
 
-AUTHORIZE_URL = 'https://api.mendeley.com/oauth/authorize'
-TOKEN_URL = 'https://api.mendeley.com/oauth/token'
-
 app = Flask(__name__)
 app.debug = True
 app.secret_key = config['clientSecret']
+
+mendeley = Mendeley(config['clientId'], config['clientSecret'], REDIRECT_URI)
 
 
 @app.route('/')
@@ -24,29 +22,21 @@ def home():
     if 'token' in session:
         return redirect('/listDocuments')
 
-    oauth = OAuth2Session(client_id=config['clientId'], redirect_uri=REDIRECT_URI, scope=['all'])
-    (login_url, state) = oauth.authorization_url(AUTHORIZE_URL)
+    auth = mendeley.start_authorization_code_flow()
+    session['state'] = auth.state
 
-    return render_template('home.html', login_url=login_url)
+    return render_template('home.html', login_url=(auth.get_login_url()))
 
 
 @app.route('/oauth')
 def auth_return():
-    if 'error_description' in request.args:
-        return render_template('error.html', error_text=request.args.get('error_description'))
+    auth = mendeley.start_authorization_code_flow(state=session['state'])
+    mendeley_session = auth.authenticate(request.url)
 
-    code = request.args.get('code')
-    oauth = OAuth2Session(client_id=config['clientId'], redirect_uri=REDIRECT_URI, scope=['all'])
+    session.clear()
+    session['token'] = mendeley_session.token
 
-    try:
-        session['token'] = oauth.fetch_token(TOKEN_URL, code=code, client_secret=config['clientSecret'])
-        return redirect('/listDocuments')
-    except OAuth2Error as e:
-        error_text = 'Error getting access token (status %s, text %s)' % (e.status_code, e.description)
-        return render_template('error.html', error_text=error_text)
-    except ValueError:
-        error_text = 'Error parsing token response.  Are your client ID/secret correct?'
-        return render_template('error.html', error_text=error_text)
+    return redirect('/listDocuments')
 
 
 @app.route('/listDocuments')
@@ -54,20 +44,12 @@ def list_documents():
     if 'token' not in session:
         return redirect('/')
 
-    oauth = OAuth2Session(client_id=config['clientId'], token=session['token'])
-    docs_response = oauth.get('https://api.mendeley.com/documents')
+    mendeley_session = get_session_from_cookies()
 
-    if not docs_response.ok:
-        return render_template('error.html', error_text='Error getting documents')
+    name = mendeley_session.profiles.me.display_name
+    docs = mendeley_session.documents.list(view='client').items
 
-    profile_response = oauth.get('https://api.mendeley.com/profiles/me')
-
-    if not profile_response.ok:
-        return render_template('error.html', error_text='Error getting profile')
-
-    name = profile_response.json()['display_name']
-
-    return render_template('library.html', name=name, docs=docs_response.json())
+    return render_template('library.html', name=name, docs=docs)
 
 
 @app.route('/document')
@@ -75,15 +57,12 @@ def get_document():
     if 'token' not in session:
         return redirect('/')
 
+    mendeley_session = get_session_from_cookies()
+
     document_id = request.args.get('document_id')
+    doc = mendeley_session.documents.get(document_id)
 
-    oauth = OAuth2Session(client_id=config['clientId'], token=session['token'])
-    doc_response = oauth.get('https://api.mendeley.com/documents/%s' % document_id)
-
-    if not doc_response.ok:
-        return render_template('error.html', error_text='Error getting document')
-
-    return render_template('metadata.html', doc=doc_response.json())
+    return render_template('metadata.html', doc=doc)
 
 
 @app.route('/metadataLookup')
@@ -91,45 +70,36 @@ def metadata_lookup():
     if 'token' not in session:
         return redirect('/')
 
+    mendeley_session = get_session_from_cookies()
+
     doi = request.args.get('doi')
+    doc = mendeley_session.catalog.by_identifier(doi=doi)
 
-    oauth = OAuth2Session(client_id=config['clientId'], token=session['token'])
-    metadata_response = oauth.get('https://api.mendeley.com/metadata?%s' % urlencode({'doi': doi}))
-
-    if metadata_response.ok:
-        catalog_id = metadata_response.json()['catalog_id']
-        response = oauth.get('https://api.mendeley.com/catalog/%s?view=all' % catalog_id)
-
-        return render_template('metadata.html', doc=response.json())
-    else:
-        return render_template('error.html', error_text='Could not find metadata for DOI %s' % doi)
+    return render_template('metadata.html', doc=doc)
 
 
-@app.route('/annotations')
-def annotations():
+@app.route('/download')
+def download():
     if 'token' not in session:
         return redirect('/')
 
+    mendeley_session = get_session_from_cookies()
+
     document_id = request.args.get('document_id')
+    doc = mendeley_session.documents.get(document_id)
+    doc_file = doc.files.list().items[0]
 
-    oauth = OAuth2Session(client_id=config['clientId'], token=session['token'])
-    annotations_response = oauth.get('https://api.mendeley.com/annotations?document_id=%s' % document_id)
-
-    if annotations_response.ok:
-        annotation_texts = []
-        for annotation in annotations_response.json():
-            if 'text' in annotation:
-                annotation_texts.append(annotation['text'])
-
-        return render_template('annotations.html', annotation_texts=annotation_texts)
-    else:
-        return render_template('error.html', error_text='Error getting annotations')
+    return redirect(doc_file.download_url)
 
 
 @app.route('/logout')
 def logout():
     session.pop('token', None)
     return redirect('/')
+
+
+def get_session_from_cookies():
+    return MendeleySession(mendeley, session['token'])
 
 
 if __name__ == '__main__':
